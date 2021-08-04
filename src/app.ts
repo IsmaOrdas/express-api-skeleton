9d580ba52b6a36b2fa3d5e9326ac2b1b5967683d
Ismael Ordás <ismael.ordas@mimacom.com>
2021-08-04 15:10:45 +0200

    Set up server

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  express.static(path.join(__dirname, "../public"), { maxAge: 31557600000 })
+);
+
+export default app;
